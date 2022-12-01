444986a5ae42e65b9b3a145234765ab0c271f8e3
funnsam <funnsam8@gmail.com>
2022-12-01 17:26:08 +0800

    day1

--- 2022/Day01-1/main.rs
+++ 2022/Day01-1/main.rs
@@ -1,2 +1,20 @@
+use std::fs;
+
 fn main() {
+    let input: String = fs::read_to_string("input.txt").unwrap();
+
+    let mut curr_cal: u64 = 0;
+    let mut cals: Vec<u64> = Vec::new();
+
+    for el in input.lines() {
+        if el == "" {
+            cals.push(curr_cal);
+            curr_cal = 0;
+        } else {
+            curr_cal += el.parse::<u64>().unwrap();
+        }
+    }
+
+    println!("{}", cals.iter().max().unwrap());
+
 }
--- 2022/Day01-2/main.rs
+++ 2022/Day01-2/main.rs
@@ -1,2 +1,22 @@
+use std::fs;
+
 fn main() {
+    let input: String = fs::read_to_string("input.txt").unwrap();
+
+    let mut curr_cal: u64 = 0;
+    let mut cals: Vec<u64> = Vec::new();
+
+    for el in input.lines() {
+        if el == "" {
+            cals.push(curr_cal);
+            curr_cal = 0;
+        } else {
+            curr_cal += el.parse::<u64>().unwrap();
+        }
+    }
+
+    cals.sort();
+    cals.reverse();
+
+    println!("{}", cals[0] + cals[1] + cals[2]);
 }
